5cc674816da754acfd2648656f0846de452a1a88
wogyt <igor.sadykov0108@gmail.com>
2025-01-03 14:54:04 +0500

    уÐкаÐк-то заработало

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.kotlin_app
 
-import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,33 +14,49 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlin_app.ui.theme.KotlinappTheme
+import java.io.Serializable
+import android.os.Parcelable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class CalculatorState (
-    var firstNumber: String = "",
-    var secondNumber: String = "",
-    var result: String = "",
-)
+    var n1s: String = "",
+    var n2s: String = "",
+    var rs: String = "",
+):Parcelable, Serializable {
+    val n1: Double get() =
+        when {
+            n1s.isEmpty() -> 0.0
+            n1s.last() == '.' -> (n1s + "0").toDouble()
+            else -> n1s.toDouble()
+        }
+    val n2: Double get() =
+        when {
+            n2s.isEmpty() -> 0.0
+            n2s.last() == '.' -> (n2s + "0").toDouble()
+            else -> n2s.toDouble()
+        }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val calculator = remember { mutableStateOf(CalculatorState()) }
             KotlinappTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
                     Calculator(
-                        calculator,
-                        activity = this,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -53,25 +69,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Calculator(
-    calculatorState: MutableState<CalculatorState>,
-    activity: MainActivity,
     modifier: Modifier = Modifier
 ) {
+    var calculator by rememberSaveable { mutableStateOf(CalculatorState()) }
+    val doublePattern = remember { Regex("""^\d+(\.\d*)?$""") }
     Column(
         modifier = modifier
     ) {
         Text("Первое число:")
-        TextField(calculatorState.value.firstNumber, onValueChange = { newValue ->
-            calculatorState.value = calculatorState.value.copy(firstNumber = newValue)
-        }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        val numberKeyboard = KeyboardOptions(
+            keyboardType = KeyboardType.Number
+        )
+        TextField(calculator.n1s, onValueChange = { newValue ->
+            if (newValue.isEmpty() || newValue.matches(doublePattern)) {
+                calculator = calculator.copy(n1s = newValue)
+            }
+        }, keyboardOptions = numberKeyboard, singleLine = true)
         Text("Второе число:")
-        TextField(calculatorState.value.secondNumber, onValueChange = { newValue ->
-            calculatorState.value = calculatorState.value.copy(secondNumber = newValue)
-        }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
+        TextField(calculator.n2s, onValueChange = { newValue ->
+            if (newValue.isEmpty() || newValue.matches(doublePattern)) {
+                calculator = calculator.copy(n2s = newValue)
+            }
+        }, keyboardOptions = numberKeyboard, singleLine = true)
+
         Button(
             onClick = {
-                val c = calculatorState.value
-                calculatorState.value = calculatorState.value.copy(result = (c.firstNumber.toDouble() + c.secondNumber.toDouble()).toString())
+                Log.d("DERP", "check out my stack trace")
+                val c = calculator
+                calculator = calculator.copy(rs = (c.n1s.toDouble() + c.n2s.toDouble()).toString())
 //                AlertDialog.Builder(activity)
 //                    .setMessage("Скоро будет считать ${calculatorState.value.firstNumber} + ${calculatorState.value.secondNumber}")
 //                    .setTitle("Проблема")
@@ -82,7 +107,7 @@ fun Calculator(
             Text("+")
         }
         Text("Результат:")
-        Text(calculatorState.value.result)
+        Text(calculator.rs)
     }
 }
 
@@ -93,3 +118,4 @@ fun GreetingPreview() {
         Text("Наш калькулятор")
     }
 }
+
